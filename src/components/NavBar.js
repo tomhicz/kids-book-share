d7b5762162544b6e7773d99bb7e0741981884d8e
Tom Hickz <999194+tomhicz@users.noreply.github.com>
2021-12-13 18:10:33 +0900

    reformat navbar based on login status

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { useAuthState } from "../firebase";
+import { getAuth, signOut } from "firebase/auth";
 import styled from "styled-components";
 
 const StyledNavBar = styled.nav`
@@ -15,6 +16,9 @@ const StyledNavBar = styled.nav`
     color: white;
     text-decoration: none;
   }
+  a.needAuth {
+    color: ${(props) => (props.isAuth ? "white" : "gray")};
+  }
 `;
 
 export default function NavBar(props) {
@@ -25,20 +29,43 @@ export default function NavBar(props) {
   //handlers
 
   return (
-    <StyledNavBar>
+    <StyledNavBar isAuth={isAuthenticated}>
       <header className="App-header">Kids Book Share Library</header>
-      <NavLink to="/">Library</NavLink> | <NavLink to="/mybooks">My Books</NavLink> |{" "}
-      <NavLink to="/users">Users</NavLink> | <NavLink to="/addbook">Add Book</NavLink> |{" "}
-      <NavLink to="/adduser">Update User</NavLink>
-      {!isAuthenticated ? (
+      <NavLink to="/">Library</NavLink> |{" "}
+      <NavLink className="needAuth" to="/mybooks">
+        My Books
+      </NavLink>{" "}
+      |{" "}
+      <NavLink className="needAuth" to="/addbook">
+        Add Book
+      </NavLink>{" "}
+      |{"   "}
+      <NavLink className="needAuth" to="/users">
+        Users
+      </NavLink>{" "}
+      |{" "}
+      <NavLink className="needAuth" to="/adduser">
+        Update User
+      </NavLink>
+      {/* {!isAuthenticated ? (
         <span>
           | <NavLink to="/login">Login</NavLink> | <NavLink to="/signup">Sign Up</NavLink>
         </span>
       ) : (
         ""
-      )}{" "}
+      )}{" "} */}
       <span>
-        &nbsp;&nbsp;&nbsp; Welcome {user?.email} (Signed in: {isAuthenticated ? "yes" : "no"} )
+        {isAuthenticated ? (
+          <span>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Welcome {user?.email.slice(0, 3)}...@...com{" "}
+            <button onClick={() => signOut(getAuth())}>Sign out</button>
+          </span>
+        ) : (
+          <span>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<NavLink to="/login">Login</NavLink> |{" "}
+            <NavLink to="/signup">Sign Up</NavLink>
+          </span>
+        )}
       </span>
     </StyledNavBar>
   );
